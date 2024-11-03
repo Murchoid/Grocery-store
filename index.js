b9b9c4d968df7001d8786380da4c0ad6f55a4d1b
Murchoid <kamaucoder@gmail.com>
2024-11-03 18:10:44 +0300

    Started serving files statically

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
+const path = require('path');
+
+
+//express middleware to serve the files statically
+app.use(express.static(path.join(__dirname, 'views','public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 //importing the file that has the function to connect to the database
 const connectDb = require('./config/connectToDb');
@@ -20,9 +26,28 @@ app.use(bodyParser.urlencoded({extended:true}));
 //making sure all routes that start with /api use the userRoutes
 app.use('/api', userRoutes);
 
+
+
+
 connectDb();
 
 
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'public','index.html'));
+})
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'public','logIn.html'));
+})
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'public','register.html'));
+})
+app.get('/checkout', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'public','checkout.html'));
+})
+app.get('/account', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'public','account.html'));
+})
 
 app.listen(port, ()=>{
     console.log("Sever listening at port "+ port);
