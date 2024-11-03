4babc231d376354a5d723f8f57f0a763185132df
Murchoid <kamaucoder@gmail.com>
2024-11-03 13:44:11 +0300

    Connected to local mongo db and installed dotenv and body-parser

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 const port = 5000;
 
 
+const connectDb = require('./config/connectToDb');
+
+connectDb();
+
 app.listen(port, ()=>{
     console.log("Sever listening at port "+ port);
 })
