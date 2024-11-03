9e36403998a81f0ac93b4bd08a92d1f00703f355
Murchoid <kamaucoder@gmail.com>
2024-11-03 12:48:18 +0300

    Initialised node and install express, mongoose and nodemon

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //this is for backend, mtu asiguze
+
+const express = require('express');
+const app = express();
+const port = 5000;
+
+
+app.listen(port, ()=>{
+    console.log("Sever listening at port "+ port);
+})
