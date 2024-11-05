0f59641f999efccdbe86cf16bbb099c9a911f98a
Murchoid <kamaucoder@gmail.com>
2024-11-05 14:48:12 +0300

    Added code to log in to connect to backend and register user

--- Backend/index.js
+++ Backend/index.js
@@ -6,11 +6,12 @@ const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 
-//express middleware to serve the files statically
-app.use(express.static(path.join(__dirname, 'views','public')));
-app.use(express.static(path.join(__dirname, 'views')));
+//express middleware to allow cross origin requests
+app.use(cors());
+
 
 //importing the file that has the function to connect to the database
 const connectDb = require('./config/connectToDb');
@@ -21,9 +22,10 @@ const itemRoutes = require('./routes/itemsRoute');
 //suing the bodyparse so that i can used the req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.options('/api', cors()); 
 
 //making sure all routes that start with /api use the userRoutes
+
 app.use('/api', userRoutes);
 app.use('/api', itemRoutes);
 
